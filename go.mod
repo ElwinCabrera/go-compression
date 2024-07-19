c1c824238468e2b358712484b11df911204cbecf
Elwin <elwincab0@gmail.com>
2024-07-19 18:38:52 -0400

    Huffman Compression and Decompression working for all bytes except byte '_' or zero because of the way I am serializing the huffman codes I am currently working on a fix that so that all bytes can be compressed

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ElwinCabrera/go-huffman
+
+go 1.22.4
+
+require github.com/ElwinCabrera/go-data-structs v1.0.1
---
+++ huffman.go
@@ -0,0 +1,159 @@
+package huffman
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/ElwinCabrera/go-data-structs/bit-structs"
+	"github.com/ElwinCabrera/go-data-structs/trees"
+	"github.com/ElwinCabrera/go-data-structs/utils"
+)
+
+func Compress(dataToCompress *[]byte) ([]byte, bool) {
+
+	canCompress := true
+
+	//Get the frequency each byte appears in the data we want to compress
+	freqMap := make(map[byte]int)
+	for _, b := range *dataToCompress {
+		freqMap[b]++
+	}
+	// Create a huffman tree and generate the corresponding huffman code for each unique byte with the weights being the frequency that each byte occurs
+	ht := trees.NewHuffmanTreeFromFrequencyMap(freqMap)
+	huffmanCodes := ht.GetHuffmanCodes()
+
+	//figure out the total bit length of the compressed data and create a new bit sequence to store the soon-to-be compressed data
+	compressedBitLen := 0
+	for _, bt := range *dataToCompress {
+		compressedBitLen += huffmanCodes[bt].GetNumBits()
+	}
+	bitSequenceOfCompressedData := bitstructs.NewBitSequence(compressedBitLen)
+
+	//For each character in the original uncompressed data append its corresponding huffman code to the bit sequence
+	compressedDataBitIdx := 0
+	for _, bt := range *dataToCompress {
+		huffmanCodeBitSeq := huffmanCodes[bt]
+		bitIdx := huffmanCodeBitSeq.GetNumBits() - 1
+		for bitIdx >= 0 {
+			bitSequenceOfCompressedData.SetBit(compressedDataBitIdx, huffmanCodeBitSeq.GetBit(bitIdx))
+			bitIdx--
+			compressedDataBitIdx++
+		}
+	}
+
+	//Finally generate a serialized version of the generated huffman code to be sent
+	//with the compressed data as these codes are the key needed for decompression.
+	serializedHuffmanCodes := serializeHuffmanCodes(huffmanCodes)
+	//compressedByteLen := len(serializedHuffmanCodes) + bitSequenceOfCompressedData.GetBytesAllocated()
+
+	//Create a buffer to hold the serialized huffman codes + the compressed data and start creating our end result
+	var compressedData bytes.Buffer
+	for _, bt := range serializedHuffmanCodes {
+		compressedData.WriteByte(bt)
+	}
+	for i := 0; i < bitSequenceOfCompressedData.GetBytesAllocated(); i++ {
+		compressedData.WriteByte(bitSequenceOfCompressedData.GetByte(i))
+	}
+
+	if compressedData.Len() >= len(*dataToCompress) {
+		//If this is the case our compression resulted in a size bigger than the length of our original data
+		//this happens because we need to also append the huffman codes with the compressed data which can sometimes add
+		//a lot more overhead to the total size of the compressed string.
+		//This is especially true for smaller data that we want to compress since in that case the huffman code overhead
+		//will almost always be longer than the compressed data or even the original data.
+		//One way we can reduce this overhead is to also huffman compress the serialized huffman codes agan and again until it cant be compressed anymore
+		canCompress = false
+		//return *dataToCompress, canCompress
+	}
+
+	numOfTrailingZerosToIgnore := uint8(bitstructs.BYTE_LENGTH - (compressedBitLen % bitstructs.BYTE_LENGTH))
+	compressedData.WriteByte(numOfTrailingZerosToIgnore)
+	return compressedData.Bytes(), canCompress
+
+}
+
+func Decompress(data *[]byte) *[]byte {
+	//recreate the original huffman codes
+	originalHuffmanCodes, serializedLen := deSerializeHuffmanCodesFromByteArray(data)
+	//serializedHuffmanCodeByteArr := (*data)[:serializedLen]
+
+	compressedData := (*data)[serializedLen : len(*data)-1]
+
+	trailingZeroBitsToRemove := (*data)[len(*data)-1]
+	bitLen := (bitstructs.BYTE_LENGTH * len(compressedData)) - int(trailingZeroBitsToRemove)
+
+	bitSequenceOfCompressedData := bitstructs.NewBitSequenceFromByteArray(&compressedData, bitLen)
+
+	hTree := trees.NewHuffmanTreeFromHuffmanCodes(originalHuffmanCodes)
+	uncompressedData := hTree.DecodeBitSequence(&bitSequenceOfCompressedData)
+
+	return uncompressedData
+}
+
+func serializeHuffmanCodes(hc map[byte]bitstructs.BitSequence) []byte {
+	//Will serialize as
+	//<char><bit_len><code-in-hex>_<char><bit_len>...<END>
+	//1 byte  1 byte      X bytes					   0x00
+	//Note: we use a bit length of 1 byte. if there's ever a case where we have a tree with depth > 255 this will fail causing incorrect data when decompressing most systems are 32 or 64 bits so a tree depth above that seems like unlikely so i think we are safe with one byte here.
+
+	var buf bytes.Buffer
+
+	fmt.Println(hc)
+	//byteToStringMap := make(map[byte]string)
+
+	for elem, bs := range hc {
+		buf.WriteByte(elem)
+		buf.WriteByte(byte(bs.GetNumBits()))
+		num := bs.GetXBytes(8)
+		str := utils.NumToHexString(uint(num))
+		buf.Write([]byte(str))
+		buf.WriteByte('_')
+	}
+	buf.Truncate(buf.Len() - 1)
+	buf.WriteByte(0x00) // signals end of huffman codes
+
+	//s := string(buf.Bytes())
+	//fmt.Println(s)
+	//fmt.Println(len(s))
+
+	return buf.Bytes()
+}
+
+func deSerializeHuffmanCodesFromByteArray(data *[]byte) (map[byte]bitstructs.BitSequence, int) {
+
+	originalHuffmanCodes := make(map[byte]bitstructs.BitSequence)
+	if len(*data) < 3 {
+		return originalHuffmanCodes, 0
+	}
+	serializedHuffmanCodeLen := 1
+	for _, bt := range *data {
+		if bt == 0 {
+			break
+		}
+		serializedHuffmanCodeLen++
+	}
+
+	idx := 0
+
+	for idx < serializedHuffmanCodeLen && (*data)[idx] != 0 {
+		ch := (*data)[idx]
+		idx++
+		bitSeq := bitstructs.NewBitSequence(int((*data)[idx]))
+		idx++
+		hexStr := ""
+		for idx < serializedHuffmanCodeLen && (*data)[idx] != 0 && (*data)[idx] != '_' {
+			hexStr += string((*data)[idx])
+			idx++
+		}
+		if idx < serializedHuffmanCodeLen && ((*data)[idx] == '_' || (*data)[idx] == 0) {
+			num := utils.HexStringToInt(hexStr)
+			bitSeq.SetBitsFromNum(0, uint64(num))
+
+			idx++
+		}
+
+		originalHuffmanCodes[ch] = bitSeq
+
+	}
+	fmt.Println(originalHuffmanCodes)
+	return originalHuffmanCodes, serializedHuffmanCodeLen
+}
